a0b66c84bd6c7725019cade28af16b0140229923
lelik25 <semernik2135@gmail.com>
2021-09-30 10:39:33 +0300

    Create Задание 2.cpp

---
+++ Задание 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int f;
+	cout << " Введите число f=";
+	cin >> f;
+	switch (f)
+	{
+	case 1: cout << "Понедельник" << endl; break;
+	case 2: cout << "Вторник" << endl; break;
+	case 3: cout << "Среда" << endl; break;
+	case 4: cout << "Четверг" << endl; break;
+	case 5: cout << "Пятница" << endl; break;
+	case 6: cout << "Суббота" << endl; break;
+	case 7: cout << "Воскресенье" << endl; break;
+	default: cout << "Не правильно введено число для выбора функции "; 
+		return 1;
+	}
+	return 0;
+}
+
