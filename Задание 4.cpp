2181aa1e350eb38bbe676287663d8ceb4248913d
lelik25 <semernik2135@gmail.com>
2021-09-30 20:17:41 +0300

    task4

---
+++ Задание 4.cpp
@@ -0,0 +1,30 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, h, r;
+	cout << "Введите длинну параллелепипеда а=" ;
+	cin >> a;
+	cout << "Введите ширину параллелепипеда b=" ;
+	cin >> b;
+	cout << "Введите высоту параллелепипеда с=" ;
+	cin >> c;
+	cout << "Введите длину отверстия h="  ;
+	cin >> h;
+	cout << "Введите высоту отверстия r=" ;
+	cin >> r;
+	switch ((b <= h) && (c <= r) ? 1 : (a <= h) && (c <= r) ? 2 : (a <= h) && (b <= r) ? 3 : (b <=r) && (a<=h)? 4 : (c<=h)&&(a<=r)? 5:(b<=r)&&(c<=h)?6: -1)
+	{
+	case 1: cout << "Проходит" << endl; break;
+	case 2: cout << "Проходит" << endl; break;
+	case 3: cout << "Проходит" << endl; break;
+	case 4: cout << "Проходит" << endl; break;
+	case 5: cout << "Проходит" << endl; break;
+	case 6: cout << "Проходит" << endl; break;
+	default: cout << "Параллелепипед не прошёл через отверстие" << endl;          
+	}
+	return 0;
+}
